08d726d3dd3a6f516043bf72f9bae636441fbbaf
smokedpirate <smokedpirate@protonmail.com>
2020-09-07 15:12:40 +0300

    removed images

--- src/faces.py
+++ src/faces.py
@@ -1,8 +1,103 @@
-
 import numpy as np
 import cv2
+import pickle
 
 face_cascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_alt2.xml')
+
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("./recognizer/face-trainner.yml")
+
+labels = {"person_name": 1}
+with open("pickles/face-labels.pickle", 'rb') as f:
+	og_labels = pickle.load(f)
+	labels = {v: k for k, v in og_labels.items()}
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]  # (ycord_start, ycord_end)
+        roi_color = frame[y:y+h, x:x+w]
+
+    	# recognize? 
+        id_, conf = recognizer.predict(roi_gray)
+        if conf >= 4 and conf <= 85:
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
+            #img_item = "7.png"
+            #cv2.imwrite(img_item, roi_color)
+
+        color = (255, 0, 0)  # BGR 0-255
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+    	#subitems = smile_cascade.detectMultiScale(roi_gray)
+    	#for (ex,ey,ew,eh) in subitems:
+    	#	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+import numpy as np
+import cv2
+import pickle
+
+face_cascade = cv2.CascadeClassifier('Cascadeshaarcascade_frontalface_alt2.xml')
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("./recognizer/face-trainner.yml")
+
+labels = {"person_name": 1}
+with open("pickles/face-labels.pickle", 'rb') as f:
+	og_labels = pickle.load(f)
+	labels = {v: k for k, v in og_labels.items()}
 
 cap = cv2.VideoCapture(0)
 
@@ -40,3 +135,4 @@ while(True):
 cap.release()
 cv2.destroyAllWindows()
 print("Execution completed")
+'''
