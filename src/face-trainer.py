8aca312818fe8979c18f04f7ee26214e9e2ce3c9
smokedpirate <smokedpirate@protonmail.com>
2020-09-18 18:43:36 +0300

    jpeg support added

--- src/face-trainer.py
+++ src/face-trainer.py
@@ -17,7 +17,7 @@ x_train = []
 
 for root, dirs, files in os.walk(image_dir):
 	for file in files:
-		if file.endswith("png") or file.endswith("jpg"):
+		if file.endswith("png") or file.endswith("jpg") or file.endswith("jpeg"):
 			path = os.path.join(root, file)
 			label = os.path.basename(root).replace(" ", "-").lower()
 			#print(label, path)
